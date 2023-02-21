d858283a30ca4795954e2bbd7362ebf67858e39c
DarinelGuillen <darinel.escobar@outlook.com>
2023-02-21 17:13:32 -0600

    [ADD] atomic desing

---
+++ src/components/atoms/FormLogin.jsx
@@ -0,0 +1,14 @@
+
+function FormLogin() {
+   
+    return (
+        <form>
+            
+            <input type="text" />
+            <input type="password"  />
+            <button >Iniciar sesión</button>
+            <button>-------</button>
+        </form>
+      );
+}
+export default FormLogin;
